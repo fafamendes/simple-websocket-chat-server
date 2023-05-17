ceeef864e6f55aa4f0930cd12366e3fd37055e19
lipegusmao <petrigusmao@gmail.com>
2023-05-17 00:24:12 -0300

    Chat container send and received msgs updated

--- src/view/static/js/index.js
+++ src/view/static/js/index.js
@@ -6,14 +6,23 @@ function sendMessage(msg) {
 }
 
 socket.on('message', (data) => {
-    console.log(data)
+    //console.log(data)
+    let msgElement = document.createElement('div')
+    msgElement.classList.add('other')
+    msgElement.innerHTML = data?.message
+    document.getElementById('chat').appendChild(msgElement)
 })
 
 document.getElementById('chat-button-send')
     .addEventListener('click', ev => {
         let chatInput = document.getElementById('chat-input-text');
         let msg = chatInput.value;
+        let msgElement = document.createElement('div')
+        msgElement.classList.add('me')
+        msgElement.innerHTML = msg
+        document.getElementById('chat').appendChild(msgElement)
         chatInput.value = '';
         msg.length && sendMessage(msg)
+        
     });
 
